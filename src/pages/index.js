664d52f46e088adfbef2a68c3fbbe5c7c8c115d5
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-09-09 11:14:42 -0300

    Mostrando la información
    desde DatoCms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from '../components/layout';
 import ImagenHotel from '../components/imagenHotel';
+import ContenidoInicifo from '../components/contenidoInicio'
 
 const IndexPage = () => (
   <Layout>
     <ImagenHotel/>
+    <ContenidoInicifo/>
   </Layout>
  
 )
